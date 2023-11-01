ecf56201dd8a38d39374d5d9d77eb59acda2799b
ana <ana@anniecannons.com>
2023-11-01 11:40:41 -0700

    commiting my assignemnt

--- algorithms/1-Strings/convertHtmlCharacters.js
+++ algorithms/1-Strings/convertHtmlCharacters.js
@@ -9,26 +9,39 @@
 // pass in a string that includes one of the following symbols: &, <, >, "", or '
 
 
-
+// this is a function that takes a parameter 
 function convertHTML(str) {
+  // setting a variable to an empty string 
   let convertedString = "";
-
+// using a for loop to loop threw the parameter in the function to find each character in the parameter str
   for (let i = 0; i < str.length; i++) {
+    // setting a variable to the  current character at index 'i'
     let char = str[i];
+    // using a if statement to see if the variable that we made equal to the index of each charter truly equal "&"
     if (char === '&') {
+      // if it is equal to "&" then add &amp;'to the new empty string and now the  variable convertedString  is now '&amp;
       convertedString += '&amp;';
+      // Check if the character is a less-than sign ('<')
     } else if (char === '<') {
+      // If it is, replace it with the HTML entity for a greater-than sign ('&lt;';)
       convertedString += '&lt;';
+      // Check if the character is a greater-than sign ('>')
     } else if (char === '>') {
+       // If it is, replace it with the HTML entity for a greater-than sign ('&gt;')
       convertedString += '&gt;';
+ // Check if the character is a double quote ('"')
     } else if (char === '"') {
+       // If it is, replace it with the HTML entity for a double quote ('&quot;')
       convertedString += '&quot;';
+      // Check if the character is a single quote (apostrophe) ("'")
     } else if (char === "'") {
+      // If it is, replace it with the HTML entity for a single quote ('&apos;')
       convertedString += '&apos;';
+      // If the character is none of the special characters above
     } else {
       convertedString += char;
     }
   }
-
+// Return the final converted string with HTML entities replaced
   return convertedString;
 }
--- algorithms/Decision-tree-graphic/decision-tree.js
+++ algorithms/Decision-tree-graphic/decision-tree.js
@@ -1,3 +1,32 @@
-function transportationOptions() {
-    // Write code here to make the decision tree into an algorithm!
+// there is weather the weather has 3 factors sun cloud and rain
+// if it is sunny then if greater than 30 min you will walk to your destination
+// if it is sunny and the time is less than 30 min then you will take the bus to your destination
+// if it is cloudy then show hungry
+// if you are hungry then walk
+// if you are not hungry then take the bus
+// if it is rain then show bus
+
+function transportationOptions(weather, isHungry, time) {
+  if (weather === "sunny" && time > 30) {
+    return "Walk to your destination it is greater than 30 min and it is sunny.";
+  } else if (weather === "sunny" && time < 30) {
+    return "Take the bus to your destination it is less than 30 min and it is sunny.";
+  } else if (weather === "cloudy" && isHungry) {
+    return "Walk to your destination because you are hungry and it is cloudy";
+  } else if (weather === "cloudy" && !isHungry) {
+    return "Catch the bus to your destination because you are not hungry and it is cloudy";
+  } else if (weather === "rain") {
+    return "Take the bus to your destination it is raining .";
+  } else {
+    return "Invalid weather condition.";
   }
+}
+
+console.log(transportationOptions, "transportationOptions");
+
+console.log(transportationOptions("sunny", true, 35)); // Output: "Walk to your destination it is greater than 30 min and it is sunny.";
+console.log(transportationOptions("sunny", true, 10)); // Output: "Take the bus to your destination it is less than 30 min and it is sunny.";
+console.log(transportationOptions("cloudy", true)); // Output: Walk to your destination because you are hungry and it is cloudy";
+console.log(transportationOptions("cloudy", false)); // Output: "Catch the bus to your destination because you are not hungry and it is cloudy";
+console.log(transportationOptions("rain")); // Output: "Take the bus to your destination it is raining .";
+console.log(transportationOptions("snow", false, 10)); // Output: Invalid weather condition.
